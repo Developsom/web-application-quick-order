b2942a244174260892aecb7619399ee78e3c01a5
Developsom <zana@cleancflo.nl>
2022-09-22 17:55:27 +0700

    Maak een component of extra functie in bestaande Productcomponent voor + en - van producten die toegevoegd kunnen worden aan winkelmandje

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import './App.css';
 import Product from "./components/Product";
-import Logo from "./assets/Logo.jpg";
+import Logo from "./assets/Logo.png";
 
 function App() {
   return (
       <>
+          <h1>Menu</h1>
+          <nav>
+          </nav>
         <main>
-            <nav>
-             <Product
-                productComment={'Best seller'}
-                img={Logo}
-                title={'Oreo Cookiez'}
-                productPrice={'500 THB'}
+                <Product
+                    productComment={'Best seller'}
+                    img={Logo}
+                    title={'Oreo Cookiez'}
+                    productPrice={'500 THB'}
                 >
-            </Product>
-            </nav>
+                </Product>
+                <Product
+                    productComment={'Up'}
+                    img={Logo}
+                    title={'Oreo Cookiez'}
+                    productPrice={'500 THB'}
+                >
+                </Product>
+                <Product
+                    productComment={'Up'}
+                    img={Logo}
+                    title={'Oreo Cookiez'}
+                    productPrice={'500 THB'}
+                >
+                </Product>
+                <Product
+                    productComment={'Up'}
+                    img={Logo}
+                    title={'Oreo Cookiez'}
+                    productPrice={'500 THB'}
+                >
+                </Product>
+                <Product
+                    productComment={'Up'}
+                    img={Logo}
+                    title={'Oreo Cookiez'}
+                    productPrice={'500 THB'}
+                >
+                </Product>
+                <Product
+                    productComment={'New'}
+                    img={Logo}
+                    title={'Oreo Cookiez'}
+                    productPrice={'500 THB'}
+                >
+                </Product>
         </main>
 
           <footer>
